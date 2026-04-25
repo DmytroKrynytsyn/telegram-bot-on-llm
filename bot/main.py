f6dbe89ce8cd6e92023d33b31e815b6c3b69e934
Dmytro Krynytsyn <dmitry.krinitsyn@gmail.com>
2026-04-25 13:41:52 +0200

    fix

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,4 @@
 import os
-import re
 import json
 import time
 import logging
@@ -17,11 +16,7 @@ class FilterHealthMetrics(logging.Filter):
 logging.getLogger("uvicorn.access").addFilter(FilterHealthMetrics())
 
 app = FastAPI()
-
-Instrumentator().instrument(
-    app,
-    should_ignore_handler=lambda req: re.search(r"/health|/metrics", req.url.path) is not None
-).expose(app)
+Instrumentator().instrument(app).expose(app)
 
 llm_request_duration = Histogram(
     "llm_request_duration_seconds",
